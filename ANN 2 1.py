f4e34bd1ae171c56af694b5959d0c91b4e6a24fe
ViksisYT <36446666+ViksisYT@users.noreply.github.com>
2019-11-06 17:35:29 +0200

    Add files via upload

---
+++ ANN 2 1.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+x = np.array([[0,0],
+              [0,1],
+              [1,0],
+              [1,1]])
+
+w = np.array([[1],
+              [1]])
+
+s = np.dot(x,w)
+
+print(s)
